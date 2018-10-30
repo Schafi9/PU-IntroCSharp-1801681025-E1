d9fcba8dd9b0e4c864e1be13bef771048735d1b4
Nedelka <neli_pz97@abv.bg>
2018-10-30 14:03:57 +0200

    test

---
+++ ExamOne1801681025/ExamOne1801681025/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExamOne1801681025
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
